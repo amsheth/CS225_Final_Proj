d3213c9c240c2af6763fb8ea1fadcf98067a3fae
Nathan Parekh <git-commits@nathanparekh.net>
2022-12-08 16:05:41 -0600

    rename c++ directory

--- c++/src/main.cpp
+++ c++/src/main.cpp
@@ -7,10 +7,11 @@ int main() {
     //Graph myGraph("rosterList.txt", "studentList.txt");
 
     //V2D roster = file_to_V2D("../rosterList.txt");
-    V2D roster = file_to_V2D("../PY/demofile3.txt");
-    //print(roster);
-    Graph abc(roster);
-    abc.displayMatrix();
-    
+//    V2D roster = file_to_V2D("../PY/demofile3.txt");
+//    //print(roster);
+//    Graph abc(roster);
+//    abc.displayMatrix();
+
+    std::cout << "hello world" << std::endl;
 
 }
