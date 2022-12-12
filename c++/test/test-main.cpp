e7ec5b8b76c2b290c26975f70749353ca5f13a5b
Nathan Parekh <git-commits@nathanparekh.net>
2022-12-12 15:12:35 -0600

    update include + cmake

--- c++/test/test-main.cpp
+++ c++/test/test-main.cpp
@@ -1,6 +1,12 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include "../src/Graph.h"
+
 TEST_CASE("Test Case 1"){
+    V2D playlist = file_to_V2D("../Python/demofile1.txt");
+
+    Graph g = Graph(playlist);
+
     int x = 0;
     REQUIRE(x==1);
 }
